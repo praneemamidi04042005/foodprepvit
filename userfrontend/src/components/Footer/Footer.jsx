5572be71e5def4c1508b22a1d404e42c38ee8d0c
praneemamidi04042005 <praneeth.23bce9683@vitapstudent.ac.in>
2025-06-30 13:05:37 +0530

    uploads

--- userfrontend/src/components/Footer/Footer.jsx
+++ userfrontend/src/components/Footer/Footer.jsx
@@ -18,10 +18,10 @@ const Footer = () => {
         <div className="footer-content-center">
           <h2>Company</h2>
           <ul>
-            <li><a href="https://faceprep.edmingle.com/"></a>Home</li>
-            <li><a href="https://faceprep.edmingle.com/contact-us"></a>About us</li>
-            <li><a href="https://faceprep.edmingle.com/courses"></a>Courses</li>
-            <li><a href="https://faceprep.edmingle.com/reviews"></a>Reviews</li>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Courses</li>
+            <li>Reviews</li>
           </ul>
         </div>
         <div className="footer-content-right">
