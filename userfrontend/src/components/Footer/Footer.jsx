2b23e0d8f0fadde77e49b976b60a36dc62277701
praneemamidi04042005 <praneeth.23bce9683@vitapstudent.ac.in>
2025-06-30 11:34:48 +0530

    Update Footer.jsx

--- userfrontend/src/components/Footer/Footer.jsx
+++ userfrontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
           <img className='footer-logo' src={assets.logo_bottom} alt="" />
-          <p>Food Prep is a full-stack project designed for hands-on teaching, helping students learn full-stack development. It's used by FACEPrep, an ed-tech company focused on equipping students with the skills to achieve their career aspirations.</p>
+          <p>Food Prep is a full-stack project designed for hands-on teaching, helping students learn full-stack development.</p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
             <img src={assets.twitter_icon} alt="" />
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>Get in touch</h2>
           <ul>
-            <li>+91 96297 45022</li>
-            <li>enquiry@faceprep.in</li>
+            <li>+91 90149 35546</li>
+            <li>praneeth.23bce9683@vitapstudent.ac.in</li>
           </ul>
         </div>
       </div>
       <hr className="footer-hr" />
-      <p className='footer-copyright'>Copyright 2024 © 2021 FoodPrep. All rights reserved.</p>
+      <p className='footer-copyright'>Copyright 2025 © 2021 FoodPrep. All rights reserved.</p>
     </div>
   )
 }
